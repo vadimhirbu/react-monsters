a65704cc15de697860ac03448264ef46b452de73
Vadim Hirbu <hirbu.vadim@gmail.com>
2020-04-14 18:33:19 +0300

    Updates

---
+++ precache-manifest.aea67b8222349050a1e8aa1c2cb48c9d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e47afb07da3c80644d46f4744129c654",
+    "url": "/react-monsters/index.html"
+  },
+  {
+    "revision": "0c2daef1fd31d6fd71c5",
+    "url": "/react-monsters/static/css/main.f35eb652.chunk.css"
+  },
+  {
+    "revision": "045c2ed955cec24ea193",
+    "url": "/react-monsters/static/js/2.6931aef1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-monsters/static/js/2.6931aef1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0c2daef1fd31d6fd71c5",
+    "url": "/react-monsters/static/js/main.f7c7a96b.chunk.js"
+  },
+  {
+    "revision": "10a25d6082fe69f22468",
+    "url": "/react-monsters/static/js/runtime-main.578e257e.js"
+  }
+]);
